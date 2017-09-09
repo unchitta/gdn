3e97dd7411bf1eb3268f714bf3afaec8ffaf9d9f
Vincent <rawpixel.vincent@gmail.com>
2017-09-09 17:11:43 +0700

    init webhook

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,12 +1,15 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import location from './location';
+import lineWebhook from './lineWebhook';
 
 export default ({ config, db }) => {
 	let api = Router();
 
 	// mount the location resource
 	api.use('/location', location({ config, db }));
+
+	api.use('/line-webhook', lineWebhook({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
---
+++ server/src/api/lineWebhook.js
@@ -0,0 +1,44 @@
+import resource from 'resource-router-middleware';
+import location from '../models/location';
+
+export default ({config, db}) => resource({
+
+  /** Property name to store preloaded entity on `request`. */
+  id: 'lineWebhook',
+
+  /** For requests with an `id`, you can auto-load the entity.
+   *  Errors terminate the request, success sets `req[id] = data`.
+   */
+  load(req, id, callback) {
+    // console.log("Location: ", req);
+    let location = location.find(location => location.id === id),
+      err = location ? null : 'Not found';
+    callback(err, location);
+  },
+
+  /** GET / - List all entities */
+  index({params}, res) {
+    db.query('SELECT * FROM datapoints', function (error, results, fields) {
+      if (error) throw error;
+      res.json(results);
+    });
+  },
+
+  /** POST / - Create a new entity */
+  create({body}, res) {
+    let vals = {
+      lineid: body.lineid,
+      // geolocation: 'PointFromText(POINT(' + body.geolocation.x + ' ' + body.geolocation.y + '))',
+      price: body.price,
+      currency: body.currency,
+      locname: body.locname,
+      time: body.time
+    };
+    let point = `POINT(${body.geolocation.x} ${body.geolocation.y})`;
+    db.query('INSERT INTO datapoints SET ?, geolocation=GeomFromText(?)', [vals, point], (err, results) => {
+      if (err) throw err;
+      console.log(body, 'BODY');
+      res.json("");
+    });
+  },
+});
