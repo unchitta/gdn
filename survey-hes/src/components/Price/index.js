9b8036914f43885c60daadd0e09f7038f9b219a6
Andrew Horn <arhorn@smcm.edu>
2017-09-09 19:30:13 +0700

    survey styling

--- survey-hes/src/components/Price/index.js
+++ survey-hes/src/components/Price/index.js
@@ -6,7 +6,13 @@ class Island extends Component {
     const { price, onChange } = this.props;
 
     return (
-      <input className="price-input" type="text" value={price} onChange={({ currentTarget: { value } }) => onChange(value)}/>
+      <div>
+        <div className="text-margin">
+          <p>Last time you bought diesel,</p>
+          <p>how much did you pay per litre?</p>
+        </div>
+        <input className="price-input" type="text" value={price} onChange={({ currentTarget: { value } }) => onChange(value)}/>
+      </div>
     );
   }
 
