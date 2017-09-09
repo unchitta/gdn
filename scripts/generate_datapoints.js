96f7199ad4eb585efcf02830487c83f586e52068
andreiashu <andreiashu@gmail.com>
2017-09-09 21:13:16 +0700

    Generation script

---
+++ scripts/generate_datapoints.js
@@ -0,0 +1,62 @@
+require('dotenv').config({
+  path: '../server/.env'
+});
+const mysql = require('mysql');
+const faker = require('faker');
+
+const db = mysql.createConnection({
+  host: process.env.GDN_DB_HOST,
+  user: process.env.GDN_DB_USER,
+  password: process.env.GDN_DB_PASS,
+  database: process.env.GDN_DB_DB
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log("Generate data....\n");
+  db.query('SELECT * FROM known_loc', (err, results) => {
+    if (err) throw err;
+
+    results.forEach((row) => {
+      if (row.geolocation === null) {
+        return;
+      }
+
+      const no = getRandomInt(2, 5);
+      for (let i = 0; i < no; i++) {
+
+        const date = faker.date.recent(200);
+        const v = {
+          lineid: faker.random.word(),
+          price: faker.finance.amount(25, 34, 2, ''),
+          currency: 'thb',
+          locname: row.name,
+          // last 200 days
+          time: `${date.getYear()}-${date.getMonth()}-${date.getDay()} ${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`,
+        };
+
+        const lat = row['geolocation']['x'];
+        const lon = row['geolocation']['y'];
+
+        let point = `POINT(${lat} ${lon})`;
+        db.query('INSERT INTO datapoints SET ?, geolocation=GeomFromText(?)', [v, point], (err, results) => {
+          if (err) throw err;
+        });
+
+      }
+    });
+  });
+});
+
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ scripts/locations_importer.js
@@ -0,0 +1,69 @@
+const sexagesimal = require('@mapbox/sexagesimal');
+require('dotenv').config({
+  path: '../server/.env'
+});
+const fs = require('fs');
+$ = jQuery = require('jquery');
+require('jquery-csv');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: process.env.GDN_DB_HOST,
+  user: process.env.GDN_DB_USER,
+  password: process.env.GDN_DB_PASS,
+  database: process.env.GDN_DB_DB
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log("Truncating table....\n");
+  db.query('TRUNCATE table known_loc', (err, results) => {
+    if (err) throw err;
+
+    console.log("Importing...\n");
+    const sample = '../data/thai-islands.csv';
+    fs.readFile(sample, 'UTF-8', function (err, csv) {
+      $.csv.toArrays(csv, {}, function (err, data) {
+        for (var i = 0, len = data.length; i < len; i++) {
+          if (i == 0) {
+            continue;
+          }
+
+          if (data[i].length != 3) {
+            continue;
+          }
+
+          if (data[i][2] == '') {
+            continue;
+          }
+
+          const latlon = data[i][2].trim().split(" ");
+          const lat = sexagesimal(latlon[0]);
+          const lon = sexagesimal(latlon[1]);
+          if (lat === "" || lon === "") {
+            console.log("Cannot parse latlon: " + latlon.join(" "));
+            continue;
+          }
+          const v = {
+            name: data[i][0].trim(),
+            thainame: data[i][1].trim()
+          };
+
+          let point = `POINT(${lat} ${lon})`;
+          db.query('INSERT INTO known_loc SET ?, geolocation=GeomFromText(?)', [v, point], (err, results) => {
+            if (err) throw err;
+          });
+        }
+      });
+    });
+  });
+});
+
+
+// console.log(sexagesimal('12.06°N') + "\n");
+// console.log(sexagesimal('98°30′02.02″E'));
+
