a41602e6af2667b007a62f7745fffc012b6a1a25
Vincent <rawpixel.vincent@gmail.com>
2017-09-09 17:31:52 +0700

    test

--- server/src/api/lineWebhook.js
+++ server/src/api/lineWebhook.js
@@ -1,5 +1,6 @@
 import resource from 'resource-router-middleware';
 import location from '../models/location';
+import { Client } from '@line/bot-sdk';
 
 export default ({config, db}) => resource({
 
@@ -14,7 +15,22 @@ export default ({config, db}) => resource({
 
   /** POST / - Create a new entity */
   create({body}, res) {
-    console.log(body);
+    
+    if (body.events && body.events.length) {
+      if (body.events[0].type === 'follow') {
+        const userId = body.events[0].source.userId;
+        const client = new Client({ 
+          channelAccessToken: 'aRYpX5jVUw532GwFclyrfEikfymSoPzGhwEG72vE+AwvrjD5cUW73rUXiyhg9GlTTSJEAYHha4Jo17X43reEJ4J7fEo8nrEYwzQ48c3NhqWNcLf6jIH4Y7opHHfit9v3DcNoEQnpuUTGkjHTh1eINgdB04t89/1O/w1cDnyilFU=',
+          channelSecret: 'bf4f42623160c813aad4cab0d3ee67b3'
+        });
+        client.pushMessage(userId, {
+          type: 'text',
+          text: 'For the sake of humanity please fill this form https://hes.delta9.link/?user=' + userId
+        });
+      }
+    }
+
+
     res.send('ok');
   },
 });
