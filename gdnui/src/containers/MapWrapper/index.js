ab62f735f98ef6e490edfa88bc60f7de3dd76583
andreiashu <andreiashu@gmail.com>
2017-09-09 20:39:35 +0700

    Import script

--- gdnui/src/containers/MapWrapper/index.js
+++ gdnui/src/containers/MapWrapper/index.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 
 class MapWrapper extends Component {
 
+  componentShouldUpdate() {
+    return false;
+  }
+
   componentDidMount() {
-    const mymap = window.L.map('mapid').setView([12.221295, 100.881145], 5);
+    this.mymap = window.L.map('mapid').setView([12.221295, 100.881145], 5);
     window.L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5kcmVpYXNodSIsImEiOiJjajdkOTNuZ3cwN245MnFvMnp2cnhjZXB3In0.nYt_dBz4wwYd9X_mP4zzVg', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      attribution: '',
       maxZoom: 18,
       id: 'mapbox.streets',
       accessToken: 'your.mapbox.access.token'
-    }).addTo(mymap);
+    }).addTo(this.mymap);
+
+    // const endpoint = 'https://hes.delta9.link/api/location';
+    const endpoint = 'http://localhost:8080/api/location';
+
+    fetch(endpoint)
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        json.forEach(point => {
+          window.L.marker([point.geolocation.x, point.geolocation.y])
+            .addTo(this.mymap)
+            .bindPopup('Price: ' + point.price + ' ' + point.currency)
+            .openPopup();
+        })
+      })
+      .catch(err => {
+        console.log('err while fetching data points', err);
+      });
   }
 
   render() {
@@ -19,7 +42,6 @@ class MapWrapper extends Component {
       </div>
     );
   }
-
 }
 
 export default MapWrapper;
